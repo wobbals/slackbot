8c3c4a1d51ec8819285b6a4375eba15033043529
Charley Robinson <charley@tokbox.com>
2014-11-11 14:54:47 -0700

    MODIFY Bot picks up jira links

--- bot.rb
+++ bot.rb
@@ -19,25 +19,12 @@ log = ChatAdapter.log
 
 # Do this thing in this block each time the bot hears a message:
 bot.on_message do |message, info|
-  # ignore all messages not directed to this bot
-  unless message.start_with? 'echobot: '
-    next # don't process the next lines in this block
-  end
-
-  # Conditionally send a direct message to the person saying whisper
-  if message == 'echobot: whisper'
-    # log some info - useful when something doesn't work as expected
-    log.debug("Someone whispered! #{info}")
-    # and send the actual message
-    bot.direct_message(info[:user], "whisper-whisper")
-  end
-
-  # split the message in 2 to get what was actually said.
-  botname, command = message.split(': ', 2)
-
-  # answer the query!
-  # this bot simply echoes the message back
-  "@#{info[:user]}: #{command}"
+  result = nil
+  log.debug (info)
+  /(?<!\/)\bOPENTOK\-\d*\b/.match(message) { |issueid|
+    result = "Did you mean https://jira.tokbox.com/browse/#{issueid} ?"
+  }
+  result
 end
 
 # actually start the bot
